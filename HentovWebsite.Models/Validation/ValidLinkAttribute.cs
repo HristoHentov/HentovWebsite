397f951a9854c11d8c7d91a9efae7f37d99968fd
Hentov <h_hentov@yahoo.com>
2017-05-09 00:50:23 +0300

    Added untracked files

---
+++ HentovWebsite.Models/Validation/ValidLinkAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using HentovWebsite.Utility;
+
+namespace HentovWebsite.Models.Validation
+{
+    public class UrlLinkAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string pattern = Consts.UrlRegex;
+            Regex reg = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            if (value != null)
+            {
+                if (reg.IsMatch((string)value))
+                    return ValidationResult.Success;
+            }
+
+            return new ValidationResult(Consts.UrlValidationError);
+
+        }
+    }
+}
---
+++ HentovWebsite.Services/Services/Contracts/IAboutService.cs
@@ -0,0 +1,10 @@
+﻿using HentovWebsite.Models.Binding.About;
+using HentovWebsite.Models.Binding.Blog;
+
+namespace HentovWebsite.Services.Services.Contracts
+{
+    public interface IAboutService : IService
+    {
+        bool SendMail(SendMailBindingModel model, string host, int port, string mailCredential, string passwordCredential, string recepient);
+    }
+}
